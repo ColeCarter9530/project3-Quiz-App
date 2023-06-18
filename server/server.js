5877b107e6d627844e93ac7f445bf35480af994e
ColeC <contactcolecarter@gmail.com>
2023-06-18 18:39:17 -0400

    created a schemas folder, with index, typeDefs, and resolvers .js files

--- server/server.js
+++ server/server.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const models = require('./models');
 const db = require("./config/connection");
-
-const app = express();
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 
 const PORT = process.env.PORT || 3001;
 
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log("Server is active on PORT 3001"); 
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const startApolloServer = async () => {
+    await startApolloServer.start();
+    apolloServer.applyMiddleware({ app });
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log("Server is active on PORT 3001"); 
+        })
     })
-})
+};
+
+startApolloServer();
+
+
